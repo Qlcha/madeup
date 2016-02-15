5af1e07439f20a7fa83cdc20a936e167c26c19f0
osharygi <olga.sphere@gmail.com>
2016-02-15 12:42:07 -0500

    configuration database

--- wp-config.php
+++ wp-config.php
@@ -28,13 +28,13 @@ define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'madeup');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'madeup_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'Jkmr1982');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
