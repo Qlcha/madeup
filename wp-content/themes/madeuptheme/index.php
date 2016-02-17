965d66fa4f60c319dad95339281841c28a03c7fe
osharygi <olga.sphere@gmail.com>
2016-02-17 10:51:40 -0500

    Comments Appearence added in file single.php

--- wp-content/themes/madeuptheme/index.php
+++ wp-content/themes/madeuptheme/index.php
@@ -1,22 +1,24 @@
 <?php get_header(); ?>
 <section>
-    <!--    Contenu des derniers articles -->
-<?php while (have_posts()) : the_post(); ?>
-    <article>
-        <h2> <?php the_title(); ?> </h2>
-        <p>  <?php the_excerpt() ?></p>
-        <p>  Article écrit par <?php the_author() ?> en date <?php the_date('') ?> </p>
+    <!-- contenu des derniers articles-->
 
-
-        <a href="<?php the_permalink() ?>" class="more">Lire la suite</a>
-        <p> <?php comments_popup_link('Aucun commentaire', '1 commentaire', '% commentaires')?></p>
-        <?php if (has_post_thumbnail()) { ?>
-        <p>  <?php the_post_thumbnail('thumbnail'); ?></p>
-        <?php } ?>
-
-    </article>
-    <?php endwhile;  ?>
-
-
+    <?php while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+            <p>Article écrit par <?php the_author(); ?> en date de <?php the_date(''); ?></p>
+            <a href="<?php the_permalink(); ?>" class="more">Lire la suite</a>
+            <p><?php comments_popup_link('Aucun commentaire', '1 commentaire', '% commentaires'); ?></p>
+            <?php if(has_post_thumbnail()) { ?>
+                <p><?php the_post_thumbnail('thumbnail'); ?></p>
+            <?php } ?>
+        </article>
+    <?php endwhile; ?>
+    <!-- instruction pour la pagination-->
+    <ul id="pagination">
+        <li><?php previous_posts_link('page précédente'); ?></li>
+        <li><?php next_posts_link('page suivante'); ?></li>
+    </ul>
 </section>
+
 <?php get_footer(); ?>
--- wp-content/themes/madeuptheme/single.php
+++ wp-content/themes/madeuptheme/single.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: osharygi
- * Date: 2016-02-16
- * Time: 08:43
- */
+<?php get_header(); ?>
+    <section>
+        <!--    Contenu des derniers articles -->
+        <?php while (have_posts()) : the_post(); ?>
+            <article>
+                <h2> <?php the_title(); ?> </h2>
+                <p>  <?php the_content() ?></p>
+                <p>  Article écrit par <?php the_author() ?> en date <?php the_date('') ?> </p>
+
+            </article>
+        <?php endwhile;  ?>
+    </section>
+<?php  comments_template(); ?>
+<?php get_footer(); ?>
